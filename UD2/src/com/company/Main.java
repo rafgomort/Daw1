05888229f02d5c81f2f3759483e8aa155ac1b5a8
Rafael Gomez <alum.rgomezo@iesalixar.org>
2019-10-03 19:02:42 +0200

    entregas

---
+++ UD2/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+
+    Scanner teclado = new Scanner(System.in);
+    int rafa = teclado.nextInt();
+    int alvaro = teclado.nextInt();
+    if (rafa > alvaro) {
+      System.out.println("Rafa es mayor");
+    } else {
+      System.out.println("Rafa es mayor");
+    }
+  }
+}
